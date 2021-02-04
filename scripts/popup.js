b60f0889a6ec3f445a500df42b02e31a4bcdc338
Yaroshevich Vlad <yaroshevich00@mail.ru>
2021-02-04 15:40:22 +0300

    v1.4

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,34 +1,20 @@
 let popupOpenBtn = document.querySelector('.profile__btn_edit');
 let popupCloseBtn = document.querySelector('.popup__close-btn');
 
-let form = document.querySelector('.form_edit');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+
+let form = document.querySelector('.form');
 let nameField = form.elements.name;
 let aboutField = form.elements.about;
 
 let popupBlock = document.querySelector('.popup');
 
-function getUserData () {
-    let userName = document.querySelector('.profile__name').textContent;
-    let userAbout = document.querySelector('.profile__about').textContent;
-
-    let userData = {userName, userAbout};
-
-    return userData;
-}
-
-function setUserData (name, about) {
-    let nameField = document.querySelector('.profile__name');
-    let aboutField = document.querySelector('.profile__about');
-
-    nameField.textContent = name;
-    aboutField.textContent = about;
-}
-
 function popupShow () {
     event.preventDefault();
 
-    nameField.value = getUserData().userName;
-    aboutField.value = getUserData().userAbout;
+    nameField.value = profileName.textContent;
+    aboutField.value = profileAbout.textContent;
 
     popupBlock.classList.add('popup_opened');
 }
@@ -45,10 +31,9 @@ function popupHide (blockName) {
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    let nameValue = nameField.value;
-    let aboutValue = aboutField.value;
+    profileName.textContent = nameField.value;
+    profileAbout.textContent = aboutField.value;
 
-    setUserData(nameValue, aboutValue);
     popupHide();
 }
 
